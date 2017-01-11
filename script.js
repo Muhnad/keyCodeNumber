53021b0d680ad2b49823305975cdce237b97dae6
Muhannad Abdelrazek <muhnad.abdelrazek@gmail.com>
2017-01-11 18:15:20 +0200

    Change code to es6

--- script.js
+++ script.js
@@ -1,56 +1,56 @@
-"use strict";
+'use strict';
 const obj = {
-    "8": "backspace",
-    "9": "Tab",
-    "13": "Enter",
-    "16": "Shift",
-    "17": "Ctrl",
-    "18": "Alt",
-    "20": "Caps Lock",
-    "27": "Esc",
-    "32": "Space",
-    "33": "Page up",
-    "34": "Page down",
-    "35": "End",
-    "36": "Home ",
-    "37": "Left",
-    "38": "Up",
-    "39": "Right",
-    "40": "Down",
-    "45": "Insert",
-    "46": "Delete",
-    "91": "Windows",
-    "106": "Multiply *",
-    "107": "Add +",
-    "109": "Subtract -",
-    "111": "Divide /",
-    "144": "NumLk",
-    "186": "Semicolon ;",
-    "187": "Equal =",
-    "188": "Comma ,",
-    "189": "Dash -",
-    "190": "Dot .",
-    "191": "Slash /",
-    "219": "Open bracket [",
-    "220": "Back slash ",
-    "221": "Close bracket ]",
-    "222": "Quote ''"
+  '8': 'backspace',
+  '9': 'Tab',
+  '13': 'Enter',
+  '16': 'Shift',
+  '17': 'Ctrl',
+  '18': 'Alt',
+  '20': 'Caps Lock',
+  '27': 'Esc',
+  '32': 'Space',
+  '33': 'Page up',
+  '34': 'Page down',
+  '35': 'End',
+  '36': 'Home ',
+  '37': 'Left',
+  '38': 'Up',
+  '39': 'Right',
+  '40': 'Down',
+  '45': 'Insert',
+  '46': 'Delete',
+  '91': 'Windows',
+  '106': 'Multiply *',
+  '107': 'Add +',
+  '109': 'Subtract -',
+  '111': 'Divide /',
+  '144': 'NumLk',
+  '186': 'Semicolon ;',
+  '187': 'Equal =',
+  '188': 'Comma ,',
+  '189': 'Dash -',
+  '190': 'Dot .',
+  '191': 'Slash /',
+  '219': 'Open bracket [',
+  '220': 'Back slash ',
+  '221': 'Close bracket ]',
+  '222': 'Quote ""'
 };
 
 
-const getCharCode = function (e) {
-    const num = e.which,
-          keynum = document.getElementById("keynum"),
-          keyword = document.getElementById("keyword"),
-          char = String.fromCharCode(num);
+const getCharCode = (e) => {
+  const num = e.which,
+        keynum = document.getElementById('keynum'),
+        keyword = document.getElementById('keyword'),
+        char = String.fromCharCode(num);
 
-    if (obj[num]) {
-        keynum.innerHTML = num;
-        keyword.innerHTML = obj[num];
-    } else {
-        keynum.innerHTML = num;
-        keyword.innerHTML = char;
-    }
-}
+  if (obj[num]) {
+      keynum.innerHTML = num;
+      keyword.innerHTML = obj[num];
+  } else {
+      keynum.innerHTML = num;
+      keyword.innerHTML = char;
+  }
+};
 
-window.onkeydown = getCharCode
+window.addEventListener('keydown', getCharCode);
